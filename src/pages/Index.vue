d081420243d0b04787caad32b49c25eac82e0aa1
Tyler Moore <tmoorewebdesigns@gmail.com>
2021-06-01 07:00:47 -0400

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -63,12 +63,12 @@ export default {
     });
   },
   data() {
-    let baseUrl = "https://walktheboro.com//statics/images";
+    let baseUrl = "https://walktheboro.com/statics/images";
     let imageURLs = [null];
     let highResImageURLs = [null];
     for (let i = 0; i < 40; i++) {
       imageURLs.push(`${baseUrl}/${i + 1}.jpg`);
-      highResImageURLs.push(`https://walktheboro.com//statics/large-images/${i + 1}.jpg`);
+      highResImageURLs.push(`https://walktheboro.com/statics/large-images/${i + 1}.jpg`);
     }
     return {
       imageURLs,
